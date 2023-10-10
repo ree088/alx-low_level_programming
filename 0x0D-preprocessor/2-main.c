d811582dbfa6ad3b7583d6e50e0d44bdd8ac2510
Reham <rehamsalah088@gmail.com>
2023-10-10 15:18:19 -0700

    D

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print name of this source file 
+ * main - prints the  name of this file 
  *
- * Return: Always 0.
+ * Return: Always 0. (Success)
  */
 int main(void)
 {
 	printf("%s\n" , __FILE__);
 	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,1 +1,1 @@
-readme
+0x0D. C - Preprocessor
