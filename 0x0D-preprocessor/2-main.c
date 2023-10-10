7ee8562e2d5f6aacc86710a1714e3d404f67a21f
Reham <rehamsalah088@gmail.com>
2023-10-10 15:29:13 -0700

    D

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the  name of this file 
+ * main - prints the name of the file 
  *
- * Return: Always 0. (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
