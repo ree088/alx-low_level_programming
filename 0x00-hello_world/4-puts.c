fc2c79937dd7a31200b5bc54d14639ced433d34a
Reham <rehamsalah088@gmail.com>
2023-10-10 12:51:24 -0700

    Helloworld

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Enter point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Prodramming is like bulding a multilingual puzzel");
+	return(0);
+}
