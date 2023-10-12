3b142ddcea489b4e05a44d4bd19e58e71f31b571
Reham <rehamsalah088@gmail.com>
2023-10-11 17:06:15 -0700

    F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
-#ifndef FUNCTION_PONTERS_H
-#define FUNCTION_PONTERS_H 
+#include "function_pointers.h"
 
-#include <stdio.h>
-
+/**
+ * prit_name - prints a name
+ * @name: the string name
+ * @f: the printing function pointer
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif 
+{
+        if (name && f)
+                 f(name);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first int 
+ * @b: the second int
+ *
+ * Retuen: the sum 
+ */
+int op_add(int a, int b)
+
+[
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Retuen: the difference
+ */
+int op_sub(int a, int b)
+{
+	return ( a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Retuen: the prodcut
+ */
+int op_mul(int a, int b)
+{ 
+	return ( a * b);
+}
+/**
+ * op_mod - modulo  two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Retuen: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+/** 
+ * op_div - multiplies two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Retuen: the quotine
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
